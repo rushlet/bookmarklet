5d1f338c33d2212d794a5fde7548860ff7cb5fe6
Becky Rush <becky.rush@bbc.co.uk>
2020-01-17 08:30:19 +0000

    fix silly mistake

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import dummyLinks from './dummy-links.js';
+import * as utils from './utils.js';
 import URLValidator from './components/url-validator/url-validator.js';
 import Bookmarks from './components/bookmarks/bookmarks.js';
 
 function init() {
     console.log('working!');
-    const links = dummyLinks;
-    localStorage.setItem('links', links);
+    if (utils.getLinksAsArray().length === 0) utils.setLinks(dummyLinks); // set up dummy links if not used before
+    const links = utils.getLinksAsArray();
     const bookmarks = new Bookmarks(links);
     const urlValidator  = new URLValidator(links, bookmarks);
     bookmarks.populateLinks();
